16414b75f7ca748440b90fe23cc54cd1d1222045
zty-f <2559247260@qq.com>
2022-02-10 21:27:16 +0800

    2022.2.10

---
+++ src/com/api/TreeSet/TreeSetDemo.java
@@ -0,0 +1,27 @@
+package com.api.TreeSet;
+
+import java.util.*;
+
+public class TreeSetDemo{
+    public static void main(String[] args){
+        //如果类和集合都带有比较规则，优先使用集合自带的比较规则
+        //Set<Student> students = new TreeSet<>(new Comparator<Student>(){
+        //    @Override
+        //    public int compare(Student o1, Student o2) {
+        //        // o1比较者   o2被比较者
+        //        return o2.getAge() - o1.getAge();//降序
+        //    }
+        //});
+
+        // lambda表达式，等同于上面代码！
+        Set<Student> students = new TreeSet<>((s1,s2)->s2.getAge()-s1.getAge());
+
+        Student s1 = new Student("a", 12);
+        Student s2 = new Student("b", 13);
+        Student s3 = new Student("c", 14);
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        System.out.println(Arrays.toString(students.toArray()));//按照年龄比较 升序
+    }
+}
---
+++ src/com/api/customgeneric/MyVariableParameter4.java
@@ -0,0 +1,51 @@
+/*
+package com.api.customgeneric;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MyVariableParameter4 {
+    public static void main(String[] args) {
+        //jdk9 新特性
+
+        // static <E>  List<E>  of(E…elements)  创建一个具有指定元素的List集合对象
+        //static <E>  Set<E>  of(E…elements)    创建一个具有指定元素的Set集合对象
+        //static <K , V>   Map<K，V>  of(E…elements) 创建一个具有指定元素的Map集合对象
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+
+    //jdk9新特性 of
+    private static void method4() {
+        Map<String, String> map = Map.of(
+                Map.entry("zhangsan", "江苏"),
+                Map.entry("lisi", "北京"));
+        System.out.println(map);
+    }
+
+    private static void method3() {
+        Map<String, String> map = Map.of("zhangsan", "江苏", "lisi", "北京");
+        System.out.println(map);
+    }
+
+    private static void method2() {
+        //传递的参数当中，不能存在重复的元素。
+        Set<String> set = Set.of("a", "b", "c", "d","a");
+        System.out.println(set);
+    }
+
+    private static void method1() {
+        List<String> list = List.of("a", "b", "c", "d");
+        System.out.println(list);
+
+        //集合的批量添加。
+        //首先是通过调用List.of方法来创建一个不可变的集合，of方法的形参就是一个可变参数。
+        //再创建一个ArrayList集合，并把这个不可变的集合中所有的数据，都添加到ArrayList中。
+        ArrayList<String> list3 = new ArrayList<>(List.of("a", "b", "c", "d"));
+        System.out.println(list3);
+    }
+}*/
