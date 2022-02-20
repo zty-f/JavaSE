1fc36f1e29c6bd1849beed30de6ecdfa30d2eeb7
zty-f <2559247260@qq.com>
2022-02-20 21:07:58 +0800

    2022.2.20

---
+++ src/com/zty/算法题测试/Test08.java
@@ -0,0 +1,30 @@
+/**
+ * @author: zty
+ * @program: JavaSE
+ * @ClassName Test08
+ * @description:
+ * @create: 2022-02-19 09:53
+ * @Version 1.0
+ **/
+package com.zty.算法题测试;
+
+import java.util.*;
+
+public class Test08 {
+    public static void main(String[] args) {
+        String a = "eat";
+        System.out.println(judge(a,"tan"));
+        HashMap<String,List<String>> map = new HashMap<>();
+        Set<String> keySet = map.keySet();
+        System.out.println(map.values());
+    }
+    public static boolean judge(String a,String b) {
+        char[] a1=a.toCharArray();
+        Arrays.sort(a1);
+        a=new String(a1);
+        char[] b1=b.toCharArray();
+        Arrays.sort(b1);
+        b=new String(b1);
+        return a.equals(b);
+    }
+}
---
+++ src/com/zty/蓝桥杯/Demo11.java
@@ -0,0 +1,41 @@
+package com.zty.蓝桥杯;
+
+import java.util.Scanner;
+
+//K好数
+/*
+如果一个自然数N的K进制表示中任意的相邻的两位都不是相邻的数字，那么我们就说这个数是K好数。
+求L位K进制数中K好数的数目。例如K = 4，L = 2的时候，所有K好数为11、13、20、22、30、31、33 共7个。
+由于这个数目很大，请你输出它对1000000007取模后的值。
+ */
+public class Demo11 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		long mod = 1000000007;
+		int radix = sc.nextInt();
+		int length = sc.nextInt();
+		long dp[][] = new long[length][radix];
+		//二维数组第一维是表示长度，第二维表示该长度下开头的数字，该数组的值为满足前二条件的K好数的个数
+		for (int i = 0; i < radix; i++) {
+			dp[0][i] = 1;
+		}
+		for (int m = 1; m < length; m++) {
+			for (int j = 0; j < radix; j++) {
+				for (int x = 0; x < radix; x++) {
+					if (x != j + 1 && x != j - 1) {
+						//如果m位长的开头为j，m-1位长开头为x，并且x与j不相临
+                        dp[m][j] += dp[m - 1][x];
+						dp[m][j] %= mod;
+					}
+				}
+			}
+		}
+		long sum = 0;
+		//由于K好数不能以0开头，所以从1开始取
+		for (int y = 1; y < radix; y++) {
+			sum += dp[length - 1][y];
+			sum %= mod;
+		}
+		System.out.println(sum);
+	}
+}
