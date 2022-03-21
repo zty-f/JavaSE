dbee8311598029a476791dda820b703cc1db4504
zty-f <2559247260@qq.com>
2022-03-21 14:42:22 +0800

    2022.3.21

---
+++ src/main/test/Test04.java
@@ -0,0 +1,22 @@
+package main.test;
+
+/**
+ * @version V1.0
+ * @Copyright swpu
+ * @author: zty-f
+ * @date: 2022/3/21 14:37
+ * @Description:  String测试
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        //String lx = "ZTY";
+        //String nb = lx;
+        //lx = "FMJ";
+        //System.out.println(nb); //ZTY
+
+        String zty = new String("ZTY");
+        String fmj = zty;
+        zty = new String("fmj");
+        System.out.println(fmj);//ZTY
+    }
+}
