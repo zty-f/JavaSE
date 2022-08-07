276a80e5d000084d635967236b28ab35aa73729c
zty-f <2559247260@qq.com>
2022-08-07 16:39:02 +0800

    手写单例模式练习

--- src/main/test03/Singleton04.java
+++ src/main/test03/Singleton04.java
@@ -9,7 +9,7 @@ package main.test03;
  * @Description: 手写单例模式04
  */
 public class Singleton04 { //静态内部类，只有调用getInstance方法的时候才会加载静态内部类，也是一次加载，同时JVM还能保证类加载时的线程安全性。  推荐使用！！！！
-    private Singleton04(){}
+    private Singleton04(){}  // 存在通过反射获取实例破坏单例模式的情况发生
 
     private static class InnerSingleton{
         private static final Singleton04 instance = new Singleton04();
--- src/main/test03/Singleton05.java
+++ src/main/test03/Singleton05.java
@@ -1,6 +1,6 @@
 package main.test03;
 
-public enum Singleton05 {
+public enum Singleton05 { //单例模式，懒加载，能够解决并发问题，同时可以防止通过反射进行篡改    推荐使用！
     INSTANCE;
 
     private static Singleton05 getInstance(){
