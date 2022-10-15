830302fe5dbd3c591f611ee35f0a8c964a91e98e
zty-f <2559247260@qq.com>
2022-10-15 21:57:05 +0800

    10.15笔试回顾学习

---
+++ src/main/test09/Test05.java
@@ -0,0 +1,21 @@
+package main.test09;
+
+/**
+ * @version V1.0
+ * @ClassName: main.test09.Test05.java
+ * @Copyright swpu
+ * @author: zty-f
+ * @date: 2022-10-15 21:31
+ * @Description:
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        long c = 123L;
+        //boolean b = null;  // 错误声明
+        //float f = 1.23; // 错误声明  默认为double，大于float，必须强转
+        float f = 1.211f;
+        System.out.println(6+5%3+2);
+        System.out.println(Integer.MAX_VALUE*2); //-2
+        System.out.println(Integer.MIN_VALUE*2); // 0
+    }
+}
