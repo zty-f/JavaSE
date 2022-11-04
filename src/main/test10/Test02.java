0e17918e6ed73f44c29999ecc8a0cc82b63eef49
zty-f <2559247260@qq.com>
2022-11-04 16:34:28 +0800

    2022.11.4

---
+++ src/main/test10/Test02.java
@@ -0,0 +1,25 @@
+package main.test10;
+
+/**
+ * @version V1.0
+ * @ClassName: main.test10.Test02.java
+ * @Copyright swpu
+ * @author: zty-f
+ * @date: 2022-10-29 17:26
+ * @Description:
+ */
+public class Test02 {
+    //public static void main(String[] args) {
+    //    System.out.println("a".toUpperCase().valueOf("b"));//b 等同于System.out.println(String.valueOf("b"));
+    //}
+    //final int  j; //成员常量必须初始化
+    public static void main(String[] args){
+        Double x=1.2;
+        //long l = 1.2; //高精度到低精度，必须强转
+        //float f =  x/l;
+        int i = 5;
+        double t = i;
+
+        //static int c = 1; //局部变量不能使用static修饰
+    }
+}
