427dfcbdbb1f12478607aabd1ce3abcdbb12c5bf
zty-f <zruler@163.com>
2022-11-29 17:18:11 +0800

    2022.5.24 11:20 忽略target包下面的代码文件，不同步到GitHub

---
+++ src/main/test10/Test04.java
@@ -0,0 +1,22 @@
+package main.test10;
+
+public class Test04 {
+    public static void main(String[] args) {
+        System.out.println("111111");
+
+        //throw new RuntimeException();
+    }
+
+    /*
+     * @MethodName:  readBook
+     * @Param null
+     * @Return 
+     * @Exception 
+     * @author: zty-f
+     * @date:  2022-11-22 9:53
+     * @Description: 
+     * */
+    public void readBook(){
+        System.out.println("read book");
+    }
+}
