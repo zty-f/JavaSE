1487baa704b97a85cf718a69a3c03568e26490a2
zty-f <zruler@163.com>
2022-11-29 18:30:56 +0800

    test

--- src/main/test10/Test04.java
+++ src/main/test10/Test04.java
@@ -4,7 +4,7 @@ public class Test04 {
     public static void main(String[] args) {
         System.out.println("111111");
 
-        //throw new RuntimeException();
+        //throw new RuntimeException();  test
     }
 
     /*
